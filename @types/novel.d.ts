4cf0a8fd5b30acf40579ef4aafb3719a79e31d37
Wambui Ndungu <wambuin18@gmail.com>
2025-03-16 11:31:05 +0300

    update @types

---
+++ @types/novel.d.ts
@@ -0,0 +1,6 @@
+   // src/@types/novel.d.ts
+   declare module 'novel/extensions' {
+    export function createSuggestionItems(items: any[]): any; // Adjust types as necessary
+    export const Command: any; // Adjust types as necessary
+    export const renderItems: any; // Adjust types as necessary
+}
